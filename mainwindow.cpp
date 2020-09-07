563ab7bbe2a04bcbd0d915e53651ba04f51e9456
Glen-tech <11800025@student.pxl.be>
2020-09-07 21:20:45 +0200

    Eerste proefcode
    - MSCV compiler dient nog geinstalleerd te worden voor test mogelijkheden

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,11 +11,48 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
-}
+   // ui->listWidget->clear();
+    connect(agent,SIGNAL(deviceDiscovered(QbluetoothDeviceInfo)), this , SLOT(deviceDiscovered(QBluetoothDeviceInfo)));
+    agent->start();
+   /* static const QString serviceUuid(QStringLiteral("00001101-0000-1000-8000-00805F9B34FB"));
+    socket = new QBluetoothSocket(QBluetoothServiceInfo::RfcommProtocol);
+    socket -> connectToService(QBluetoothAddress(string),QBluetoothUuid(serviceUuid), QIODevice::ReadWrite);*/
+
+   }
+
 
 void MainWindow::on_Voorruit_clicked()
 {
     ui->Sensor1->setText("Testing succes");
+}
+
+
+void MainWindow::deviceDiscoverd(const QBluetoothDeviceInfo &device)
+{
+    //ui -> listWidget->addItem (device.address().toString());
+    qDebug() << "Found new device:" << device.name() << '(' << device.address().toString() << ')';
+}
+
+
+void MainWindow::on_On_clicked()
+{
+   // socket->write("h");
+ }
+
+void MainWindow::on_Off_clicked()
+{
+  //  socket->write("b");
+}
+
+void MainWindow::on_Find_clicked()
+{
+   /* ui->listWidget->show();
+    ui -> listWidget->clear();
+    agent -> stop();
+    agent->start();*/
+
+  //  agent->scan()->setEnabled(true);
+  //  ui->inquiryType->setEnabled(false);
 }
 
 void MainWindow::on_Links_clicked()
@@ -32,3 +69,11 @@ void MainWindow::on_Achteruit_clicked()
 {
 
 }
+
+void MainWindow::on_listWidget_itemClicked(QListWidgetItem *item)
+{
+    string = item -> text();
+}
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QBluetoothDeviceDiscoveryAgent>
+#include <QListWidget>
+#include <QBluetoothSocket>
+#include <QString>
 
 namespace Ui {
 class MainWindow;
@@ -24,8 +28,22 @@ private slots:
 
     void on_Achteruit_clicked();
 
+    void deviceDiscoverd(const QBluetoothDeviceInfo &device);
+
+    void on_On_clicked();
+
+    void on_Off_clicked();
+
+    void on_Find_clicked();
+
+    void on_listWidget_itemClicked(QListWidgetItem *item);
+
 private:
     Ui::MainWindow *ui;
+    QBluetoothDeviceDiscoveryAgent *agent = new QBluetoothDeviceDiscoveryAgent(this);
+    //QBluetoothSocket *socket;
+    QString string ;
+  //  QBluetoothDeviceInfo *device = new QBluetoothDeviceInfo;
 };
 
 #endif // MAINWINDOW_H
